e9540fb7299ce63de4fb140596a5ca98dc87cf47
Rogerio Andre Queke Junior <junior_queke1@hotmail.com>
2020-09-02 17:51:30 -0300

    Adição da classe ForEach.java, aprendendo a utilizar o foreach para percorrer um array

--- javaUdemy/src/arrays/ForEach.java
+++ javaUdemy/src/arrays/ForEach.java
@@ -3,8 +3,18 @@ package arrays;
 public class ForEach {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double notas[] = {9.9,8.2,7.34,7.13};
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.print(notas[i]+" ");
+		}
+		System.out.println();
+		for (double nota : notas) {
+			System.out.print(nota+ " foreach ");
+		}
+		
+	
 	}
 
 }
